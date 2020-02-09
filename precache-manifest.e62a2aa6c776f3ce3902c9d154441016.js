9716ec41669f833a1e3a13830ca5ef167338ebf2
Florian Ipate <florianipate@Florians-iMac.local>
2020-02-09 17:40:49 +0000

    Updates

---
+++ precache-manifest.e62a2aa6c776f3ce3902c9d154441016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a01b29a0b165e3ea471d39f35adb0d0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2db3c9904d3018bb8564",
+    "url": "/monster-rolodex/static/css/main.ed5f0a6a.chunk.css"
+  },
+  {
+    "revision": "4beba386462ee8d71d1c",
+    "url": "/monster-rolodex/static/js/2.a099693a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.a099693a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db3c9904d3018bb8564",
+    "url": "/monster-rolodex/static/js/main.e5b5a335.chunk.js"
+  },
+  {
+    "revision": "7c51601f3c41d310bcfd",
+    "url": "/monster-rolodex/static/js/runtime-main.a8330fe5.js"
+  }
+]);
